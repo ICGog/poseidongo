ba83be96a078148e72951f6b03c6cd7e54b26fb8
Ionel Gog <gogionel@gmail.com>
2017-04-27 16:26:38 +0300

    Updated k8sclient.

--- pkg/k8sclient/k8sclient.go
+++ pkg/k8sclient/k8sclient.go
@@ -20,95 +20,55 @@ package k8sclient
 
 import (
 	"fmt"
-	"time"
 
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/api"
+	"k8s.io/client-go/pkg/fields"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
-
-type Pod struct {
-	ID string
-}
 
 type Node struct {
 	ID string
 }
 
-// type Client struct {
-// 	k8sApi *k8sClient.Client
-// 	nodeCh chan *Node
-// 	podCh  chan *Pod
-// }
+type Pod struct {
+	ID string
+}
 
-// func StartNodeWatcher(clientset Clientset) (chan *Node, chan struct{}) {
-// 	nodeCh := make(chan *Node, NodeBufferSize)
+func StartNodeWatcher(clientset *kubernetes.Clientset) chan *Node {
+	nodeCh := make(chan *Node, 100)
+	nodeListWatcher := cache.NewListWatchFromClient(clientset.Core().RESTClient(), "nodes", api.NamespaceDefault, fields.Everything())
+	_, nodeInformer := cache.NewInformer(
+		nodeListWatcher,
+		&api.Node{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(nodeObj interface{}) {
+				node := nodeObj.(*api.Node)
+				if node.Spec.Unschedulable {
+					return
+				}
+				nodeCh <- &Node{
+					ID: node.Name,
+				}
+			},
+			UpdateFunc: func(oldNodeObj, newNodeObj interface{}) {},
+			DeleteFunc: func(nodeObj interface{}) {},
+		},
+	)
+	stopCh := make(chan struct{})
+	go nodeInformer.Run(stopCh)
+	return nodeCh
+}
 
-// _, nodeInformer := framework.NewInformer(
-// 	cache.NewListWatchFromClient(c, "nodes", api.NamespaceAll,
-// 		fields.ParseSelectorOrDie("")),
-// 	&api.Node{},
-// 	0,
-// 	framework.ResourceEventHandlerFuncs{
-// 		AddFunc: func(nodeObj interface{}) {
-// 			node := nodeObj.(*api.Node)
-// 			if node.Spec.Unschedulable {
-// 				return
-// 			}
-// 			nodeCh <- &Node{
-// 				ID: node.Name,
-// 			}
-// 		},
-// 		UpdateFunc: func(oldNodeObj, newNodeObj interface{}) {},
-// 		DeleteFunc: func(nodeObj interface{}) {},
-// 	},
-// )
-//	stopCh := make(chan struct{})
-//	go nodeInformer.Run(stopCh)
-//	return nodeCh, stopCh
-//}
-
-// func StartPodWatcher() (chan *Pod, chan struct{}) {
-// 	podCh := make(chan *Pod, PodBufferSize)
-
-// 	podSelector := fields.ParseSelectorOrDie("spec.nodeName==")
-// 	podInformer := framework.NewSharedInformer(
-// 		&cache.ListWatch{
-// 			ListFunc: func(options api.ListOptions) (runtime.Object, error) {
-// 				options.FieldSelector = podSelector
-// 				return c.Pods(api.NamespaceAll).List(options)
-// 			},
-// 			WatchFunc: func(options api.ListOptions) (watch.Interface, error) {
-// 				options.FieldSelector = podSelector
-// 				return c.Pods(api.NamespaceAll).Watch(options)
-// 			},
-// 		},
-// 		&api.Pod{},
-// 		0,
-// 	)
-// 	podInformer.AddEventHandler(framework.ResourceEventHandlerFuncs{
-// 		AddFunc: func(podObj interface{}) {
-// 			pod := podObj.(*api.Pod)
-// 			podeCh <- &Pod{
-// 				ID: path.Join(pod.Namespace, pod.Name),
-// 			}
-// 		},
-// 		UpdateFunc: func(oldPodObj, newPodObj interface{}) {},
-// 		DeleteFunc: func(podObj interface{}) {},
-// 	})
-// 	stopCh := make(chan struct{})
-// 	go podInformer.Run(stopCh)
-// 	return podCh, stopCh
-// }
+func StartPodWatcher(clientset *kubernetes.Clientset) chan *Pod {
+	podCh := make(chan *Pod, 100)
+	//	stopCh := make(chan struct{})
+	return podCh
+}
 
 func New(kubeConfig string) (int, int) {
-	// nodeCh, stopNodeCh = StartNodeWatcher()
-	// podCh, stopPodCh = StartPodWatcher()
-	// return &Client{
-	// 	k8sApi: c,
-	// 	nodeCh: nodeCh,
-	// 	podCh:  podCh,
-	// }, nil
 	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
 	if err != nil {
 		panic(err.Error())
@@ -117,14 +77,32 @@ func New(kubeConfig string) (int, int) {
 	if err != nil {
 		panic(err.Error())
 	}
+	nodeCh := StartNodeWatcher(clientset)
+	podCh := StartPodWatcher(clientset)
 	for {
-		pods, err := clientset.Core().Pods("").List(v1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
+		select {
+		case <-nodeCh:
+			fmt.Println("New node")
+		case <-podCh:
+			fmt.Println("New pod")
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-		time.Sleep(10 * time.Second)
 	}
+
+	// podListWatcher := cache.NewListWatchFromClient(clientset.Core().RESTClient(), "pods", api.NamespaceDefault, fields.Everything())
+	// cache.NewIndexerInformer(podListWatcher, &v1.Pod{}, 0, cache.ResourceEventHandlerFuncs{
+	// 	AddFunc:    func(obj interface{}) {},
+	// 	UpdateFunc: func(oldObj, newObj interface{}) {},
+	// 	DeleteFunc: func(obj interface{}) {},
+	// }, cache.Indexers{})
+
+	// for {
+	// 	pods, err := clientset.Core().Pods("").List(v1.ListOptions{})
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+	// 	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	// 	time.Sleep(10 * time.Second)
+	// }
 
 	return 1, 1
 }
