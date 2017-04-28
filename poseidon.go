9e7e7d7adb921c89fa2115e11a7b4f6d592b5b2a
Ionel Gog <gogionel@gmail.com>
2017-04-28 10:43:10 +0300

    Method to create resource topology for node.

--- poseidon.go
+++ poseidon.go
@@ -32,11 +32,42 @@ var (
 )
 
 func init() {
-
 	flag.StringVar(&firmamentAddress, "firmamentAddress", "127.0.0.1:9090", "Firmament scheduler address")
 	flag.StringVar(&kubeConfig, "kubeConfig", "kubeconfig.cfg", "Path to the kubeconfig file")
 	flag.Parse()
 }
+
+func GenerateResourceID() string {
+	return "test"
+}
+
+// func createResourceTopologyForNode(hostname string, cpuCapacity int, ramCapacity int) *ResourceTopologyNodeDescriptor {
+// 	resUuid := GenerateResourceID()
+// 	rtnd := &firmament.ResourceTopologyNodeDescriptor{
+// 		Uuid:         resUuid,
+// 		Type:         firmament.ResourceDescriptor_ResourceMachine,
+// 		State:        firmament.ResourceDescriptor_ResourceIdle,
+// 		FriendlyName: hostname,
+// 		ResourceCapacity: &firmament.ResourceVector{
+// 			RamCap:   ramCapacity,
+// 			CpuCores: cpuCapacity,
+// 		},
+// 	}
+// 	// TODO(ionel): In the future, we want to get real node topology rather
+// 	// than manually connecting PU RDs to the machine RD.
+// 	for num_pu := 0; num_pu < cpuCapacity; num_pu++ {
+// 		puResUuid := GenerateResourceID()
+// 		puRtnd := &firmament.ResourceTopologyNodeDescriptor{
+// 			UUid:         puResUuid,
+// 			Type:         firmament.ResourceDescriptor_ResourcePu,
+// 			State:        firmament.ResourceDescriptor_ResourceIdle,
+// 			FriendlyName: hostname + "_pu" + strconv.Itoa(num_pu),
+// 			ParentId:     resUuid,
+// 		}
+// 		rtnd.Children = append(rntd.Children, puRtnd)
+// 	}
+// 	return rtnd
+// }
 
 // func createNewJob(controllerId string) {
 // 	// jobDesc := firmament.JobDescriptor{
