290e1a272c9e58110b6ba5640b32248b92e68ee2
Ionel Gog <gogionel@gmail.com>
2017-05-11 13:54:08 +0100

    Added error checking for stream send in stats server.

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -67,68 +67,56 @@ func convertNodeStatsToResourceStats(nodeStats *NodeStats) *firmament.ResourceSt
 		MemReservation: nodeStats.GetMemReservation(),
 		MemUtilization: nodeStats.GetMemUtilization(),
 	}
-
 }
 
 func (s *poseidonStatsServer) ReceiveNodeStats(stream PoseidonStats_ReceiveNodeStatsServer) error {
-
-	// create a response stream to reply to client
-	nodeStatResp := &poseidonStatsReceiveNodeStatsServer{stream}
-
 	for {
 		nodeStats, err := stream.Recv()
 		if err == io.EOF {
-
-			//no valid nodeStats object here so return nothing
-			glog.Info("Consumed all the node info from stats client")
-
+			glog.Info("Consumed all node stats from client")
 			return nil
 		}
 		if err != nil {
-			//TODO:(shiv) return the correct error
-			glog.Errorln("Stream error on nodestats receive ", err)
+			glog.Errorln("Stream error in node stats receive ", err)
 			return err
 		}
-
 		resourceStats := convertNodeStatsToResourceStats(nodeStats)
 		rtnd, ok := k8sclient.NodeToRTND[nodeStats.GetHostname()]
 		if !ok {
-			// TODO(ionel): Handle case when node does not have an associated resource id.
-			nodeStatResp.Send(&NodeStatsResponse{
+			sendErr := stream.Send(&NodeStatsResponse{
 				Type:     NodeStatsResponseType_NODE_NOT_FOUND,
 				Hostname: nodeStats.GetHostname(),
 			})
+			if sendErr != nil {
+				glog.Error("Stream send error in node stats receive ", sendErr)
+				return sendErr
+			}
 			continue
-
 		}
 		resourceStats.ResourceUid = &firmament.ResourceUID{
 			ResourceUid: rtnd.GetResourceDesc().GetUuid(),
 		}
 		firmament.AddNodeStats(s.firmamentClient, resourceStats)
-		nodeStatResp.Send(&NodeStatsResponse{
+		sendErr := stream.Send(&NodeStatsResponse{
 			Type:     NodeStatsResponseType_NODE_STATS_OK,
 			Hostname: nodeStats.GetHostname(),
 		})
-
+		if sendErr != nil {
+			glog.Error("Stream send error in node stats receive ", sendErr)
+			return sendErr
+		}
 	}
 }
 
 func (s *poseidonStatsServer) ReceivePodStats(stream PoseidonStats_ReceivePodStatsServer) error {
-
-	//create a
-	podStatsResp := &poseidonStatsReceivePodStatsServer{stream}
 	for {
 		podStats, err := stream.Recv()
 		if err == io.EOF {
-			//Here we dont have a valid pod object so return nothing to client
-			// Do we need to close the stream?
-			glog.Info("Consumed all message from client")
-
+			glog.Info("Consumed all pod stats from client")
 			return nil
 		}
 		if err != nil {
-			//here we dont have a valid pod object so return nothing
-			glog.Error("Stream error in ReceivePodStats", err)
+			glog.Error("Stream receive error in pod stats receive ", err)
 			return err
 		}
 		taskStats := convertPodStatsToTaskStats(podStats)
@@ -138,25 +126,30 @@ func (s *poseidonStatsServer) ReceivePodStats(stream PoseidonStats_ReceivePodSta
 		}
 		td, ok := k8sclient.PodToTD[podIdentifier]
 		if !ok {
-			// TODO(ionel): Handle case when pod does not have an associated task id.
-			podStatsResp.Send(&PodStatsResponse{
+			sendErr := stream.Send(&PodStatsResponse{
 				Type:      PodStatsResponseType_POD_NOT_FOUND,
 				Name:      podStats.GetName(),
 				Namespace: podStats.GetNamespace(),
 			})
+			if sendErr != nil {
+				glog.Error("Stream send error in pod stats receive ", sendErr)
+				return sendErr
+			}
 			continue
 		}
 		taskStats.TaskUid = &firmament.TaskUID{
 			TaskUid: td.GetUid(),
 		}
 		firmament.AddTaskStats(s.firmamentClient, taskStats)
-		//Do we need to send response for all successful call?
-		podStatsResp.Send(&PodStatsResponse{
+		sendErr := stream.Send(&PodStatsResponse{
 			Type:      PodStatsResponseType_POD_STATS_OK,
 			Name:      podStats.GetName(),
 			Namespace: podStats.GetNamespace(),
 		})
-
+		if sendErr != nil {
+			glog.Error("Stream send error in pod stats receive ", sendErr)
+			return sendErr
+		}
 	}
 }
 
